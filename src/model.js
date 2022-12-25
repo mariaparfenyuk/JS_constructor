5b7dd37a251d3f6054631bb9cc35f96f92a0f56f
mariaparfenyuk <umme0807@gmail.com>
2022-12-25 19:29:23 +0100

    add scripts and change structure

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+  {type: 'title', value: 'Hello world from JS'},
+  {type: 'text', value: 'lorem'},
+  {type: 'columns', value: [
+    '11111',
+    '22222',
+    '33333'
+  ]},
+  {type: 'image', value: './assets/image.jpg'},
+]
