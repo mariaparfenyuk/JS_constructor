5368130fe96934021be0b2f409b0c832b6df676d
mariaparfenyuk <umme0807@gmail.com>
2022-12-26 19:22:49 +0100

    update templates

--- src/model.js
+++ src/model.js
@@ -1,10 +1,14 @@
+import image from './assets/image.jpg'
+
 export const model = [
-  {type: 'title', value: 'Hello world from JS'},
+  {type: 'title', value: 'Hello world from JS', options: {
+    
+  }},
   {type: 'text', value: 'lorem'},
   {type: 'columns', value: [
     '11111',
     '22222',
     '33333'
   ]},
-  {type: 'image', value: './assets/image.jpg'},
+  {type: 'image', value: image},
 ]
