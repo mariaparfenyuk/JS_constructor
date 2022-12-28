04174e81a7dfa32160ab52400c05f887f03c5ed0
mariaparfenyuk <umme0807@gmail.com>
2022-12-28 19:56:30 +0100

    add styles

--- src/model.js
+++ src/model.js
@@ -1,8 +1,9 @@
 import image from './assets/image.jpg'
 
 export const model = [
-  {type: 'title', value: 'Hello world from JS', options: {
-    
+  {type: 'title', value: 'Website builder', options: {
+    tag: 'h2',
+    styles: `background: linear-gardient(to right, #ff0099, #493240);color: #fff;text-align: center; padding: 1.5rem`
   }},
   {type: 'text', value: 'lorem'},
   {type: 'columns', value: [
